ac7cb301d3368d73a48f3730644741cfa7456775
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-04-27 15:28:30 +0200

    EmptyIfNullExtensions, deleted 'EmptyIfNull<T>(T[])' method because this case is already covered by 'EmptyIfNull<T>(IReadOnlyList<T>)'

--- Collections/EmptyIfNullExtensions.cs
+++ Collections/EmptyIfNullExtensions.cs
@@ -1,19 +1,13 @@
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Xmf2.Common.Collections
 {
 	public static class EmptyIfNullExtensions
 	{
-		public static T[] EmptyIfNull<T>(this T[] source)
-		{
-			return source ?? EmptyArray<T>.Singleton;
-		}
-
 		public static IReadOnlyList<T> EmptyIfNull<T>(this IReadOnlyList<T> source)
 		{
-			return source ?? EmptyList<T>.Singleton;
+			return source ?? EmptyArray<T>.SINGLETON;
 		}
 
 		public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> source)
@@ -23,12 +17,7 @@ namespace Xmf2.Common.Collections
 
 		private static class EmptyArray<T>
 		{
-			public static readonly T[] Singleton = new T[0];
-		}
-
-		private static class EmptyList<T>
-		{
-			public static readonly ReadOnlyCollection<T> Singleton = new List<T>(0).AsReadOnly();
+			public static readonly T[] SINGLETON = new T[0];
 		}
 	}
 }
