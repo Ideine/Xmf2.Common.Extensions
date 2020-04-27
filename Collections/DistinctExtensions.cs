82fd95a2e5f550137edd2de85381241fb47ad956
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-04-27 16:02:40 +0200

    Fix DistinctExtensions class name

--- Collections/DistinctExtensions.cs
+++ Collections/DistinctExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class IEnumerableExtensions
+public static class DistinctExtensions
 {
 	public static IEnumerable<TResult> Distinct<T, TResult>(this IEnumerable<T> source, Func<T, TResult> selector)
 		=> source.Select(selector).Distinct();
