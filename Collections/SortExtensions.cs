cbc6f2a4edb05862d0f52f882533705e048ed0f9
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-04-27 15:16:13 +0200

    Updated SortMethod

--- Collections/SortExtensions.cs
+++ Collections/SortExtensions.cs
@@ -5,9 +5,16 @@ namespace Xmf2.Common.Collections
 {
 	public static class SortExtensions
 	{
-		public static List<T> Sort<T, TU>(this List<T> list, Func<T, TU> sortBy)
+		[Obsolete("Use SortBy.")]//Obsolete because 'Sort' metho already exists on List, therefore IDE is not able to suggest to add this Extension namespace automatically wich is pain in... uhh, somewhere.
+		public static List<T> Sort<T, TU>(this List<T> list, Func<T, TU> sortBySelector)
 		{
-			list.Sort((a, b) => Comparer<TU>.Default.Compare(sortBy(a), sortBy(b)));
+			list.Sort((a, b) => Comparer<TU>.Default.Compare(sortBySelector(a), sortBySelector(b)));
+			return list;
+		}
+
+		public static List<T> SortBy<T, TU>(this List<T> list, Func<T, TU> sortBySelector)
+		{
+			list.Sort((a, b) => Comparer<TU>.Default.Compare(sortBySelector(a), sortBySelector(b)));
 			return list;
 		}
 	}
