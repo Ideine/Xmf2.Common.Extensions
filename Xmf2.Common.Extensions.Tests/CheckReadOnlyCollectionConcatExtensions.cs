9c1497201fcf14be3a46d9b4f8ac39dfd89ee974
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-04-30 08:32:35 +0200

    Added unit tests

---
+++ Xmf2.Common.Extensions.Tests/CheckReadOnlyCollectionConcatExtensions.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Xmf2.Common.Collections;
+using Xunit;
+
+namespace Xmf2.Common.Extensions.Tests
+{
+	public static class CheckReadOnlyCollectionConcatExtensions
+	{
+		[Fact]
+		public static void ShouldConcat()
+		{
+			IReadOnlyList<int> a = new int[] { };
+			IReadOnlyList<int> b = new int[] { 0, 1, 2 };
+			CheckAssertions(onEnum: a.Concat(b));
+
+			a = new int[] { 0};
+			b = new int[] { 1, 2 };
+			CheckAssertions(onEnum: a.Concat(b));
+
+			a = new int[] { 0, 1 };
+			b = new int[] { 2 };
+			CheckAssertions(onEnum: a.Concat(b));
+
+			a = new int[] { 0, 1, 2 };
+			b = new int[] { };
+			CheckAssertions(onEnum: a.Concat(b));
+
+			void CheckAssertions(IReadOnlyList<int> onEnum)
+			{
+				//Check indexer
+				for (int i = 0 ; i < 3 ; i++)
+				{
+					Assert.Equal(i, onEnum[i]);
+				}
+
+				//Check count
+				Assert.Equal(3, onEnum.Count);
+
+				//Check sequence
+				Assert.Equal(new int[] { 0, 1, 2 }, onEnum);
+
+				//Check enumerator
+				int j = 0;
+				foreach (var item in onEnum)
+				{
+					Assert.Equal(j++, item);
+				}
+			}
+		}
+	}
+}
