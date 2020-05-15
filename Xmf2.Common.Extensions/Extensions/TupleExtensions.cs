e54fb0ad93ce50b8c6d95a07ef3ebdef87e8ed38
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-05-15 14:19:24 +0200

    Added TupleExtensions.ToArguments

---
+++ Xmf2.Common.Extensions/Extensions/TupleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Xmf2.Common.Extensions
+{
+	public static class TupleExtensions
+	{
+		public static TOut ToArguments<TIn1, TIn2, TOut>(this Tuple<TIn1, TIn2> tuple, Func<TIn1, TIn2, TOut> func)
+			=> func(tuple.Item1, tuple.Item2);
+
+		public static TOut ToArguments<TIn1, TIn2, TIn3, TOut>(this Tuple<TIn1, TIn2, TIn3> tuple, Func<TIn1, TIn2, TIn3, TOut> func)
+			=> func(tuple.Item1, tuple.Item2, tuple.Item3);
+
+		public static TOut ToArguments<TIn1, TIn2, TIn3, TIn4, TOut>(this Tuple<TIn1, TIn2, TIn3, TIn4> tuple, Func<TIn1, TIn2, TIn3, TIn4, TOut> func)
+			=> func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4);
+
+		public static TOut ToArguments<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this Tuple<TIn1, TIn2, TIn3, TIn4, TIn5> tuple, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> func)
+			=> func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5);
+	}
+}
