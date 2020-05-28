9405606e6a33e7781a79bb8785c4f0417304d7db
Thierry Levisse <thierry.levisse@ideine.fr>
2020-05-28 14:34:06 +0200

    Handle DateTimeOffset

--- Xmf2.Common.Extensions/Extensions/DateExtensions.cs
+++ Xmf2.Common.Extensions/Extensions/DateExtensions.cs
@@ -54,6 +54,57 @@ namespace Xmf2.Common.Extensions
 			return firstDate.Year == secondDate.Year;
 		}
 
+		#region DateTimeOffset
+
+		public static DateTime BeginningOfDay(this DateTimeOffset date) => date.Date;
+
+		public static DateTime EndOfDay(this DateTimeOffset date)
+		{
+			var endOfDay = new DateTime(date.Year, date.Month, date.Day);
+			endOfDay = endOfDay.AddDays(1);
+			endOfDay = endOfDay.AddTicks(-1);
+
+			return endOfDay;
+		}
+
+		public static DateTime BeginningOfMonth(this DateTimeOffset date)
+		{
+			return new DateTime(date.Year, date.Month, 1);
+		}
+
+		public static DateTime BeginningOfWeek(this DateTimeOffset date, DayOfWeek starDayOfWeek = DayOfWeek.Monday)
+		{
+			int diff = (7 + (date.DayOfWeek - starDayOfWeek)) % 7;
+			return date.AddDays(-1 * diff).Date;
+		}
+
+		public static bool IsSameDay(this DateTimeOffset firstDate, DateTimeOffset secondDate)
+		{
+			return firstDate.IsSameMonth(secondDate) && firstDate.Day == secondDate.Day;
+		}
+
+		public static bool IsSameWeek(this DateTimeOffset firstDate, DateTimeOffset secondDate, DayOfWeek startDayOfWeek = DayOfWeek.Monday)
+		{
+			DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+
+			int firstDateWeek = dfi.Calendar.GetWeekOfYear(firstDate.Date, dfi.CalendarWeekRule, startDayOfWeek);
+			int secondDateWeek = dfi.Calendar.GetWeekOfYear(secondDate.Date, dfi.CalendarWeekRule, startDayOfWeek);
+
+			return firstDateWeek == secondDateWeek;
+		}
+
+		public static bool IsSameMonth(this DateTimeOffset firstDate, DateTimeOffset secondDate)
+		{
+			return firstDate.IsSameYear(secondDate) && firstDate.Month == secondDate.Month;
+		}
+
+		public static bool IsSameYear(this DateTimeOffset firstDate, DateTimeOffset secondDate)
+		{
+			return firstDate.Year == secondDate.Year;
+		}
+
+		#endregion
+
 		public static IEnumerable<DateTimeOffset> Range(DateTimeOffset from, DateTimeOffset to, TimeSpan timeSpan)
 		{
 			for (DateTimeOffset dayIndex = from ; dayIndex <= to ; dayIndex = dayIndex.Add(timeSpan))
