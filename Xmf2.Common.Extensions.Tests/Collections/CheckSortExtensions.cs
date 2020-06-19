c3321facdcdf9ec3600deb544967f13b5d39cfd2
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-06-19 14:03:18 +0200

    Added list.Sort(...) extensions

---
+++ Xmf2.Common.Extensions.Tests/Collections/CheckSortExtensions.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Xmf2.Common.Collections;
+using Xunit;
+
+namespace Xmf2.Common.Tests.Collections
+{
+	public class CheckSortExtensions
+	{
+		[Fact]
+		public static void SortByMultipleComparerWorks()
+		{
+			List<Foo> list;
+
+			list = GetTestList();
+			list.Sort(
+				(x, y) => Comparer<int>.Default.Compare(x.SomeInt, y.SomeInt),
+				(x, y) => Comparer<string>.Default.Compare(x.SomeString, y.SomeString)
+			);
+			Assert.Equal(
+				new[] { "1a", "1b", "2a", "2b" },
+				list.ToArray(x => $"{x.SomeInt}{x.SomeString}")
+			);
+
+
+			list = GetTestList();
+			list.Sort(
+				(x, y) => Comparer<string>.Default.Compare(x.SomeString, y.SomeString),
+				(x, y) => Comparer<int>.Default.Compare(x.SomeInt, y.SomeInt)
+			);
+			Assert.Equal(
+				new[] { "a1", "a2", "b1", "b2" },
+				list.ToArray(x => $"{x.SomeString}{x.SomeInt}")
+			);
+
+
+			list = GetTestList();
+			list.Sort(
+				(x, y) => Comparer<int>.Default.Compare(x.SomeInt, y.SomeInt),
+				(x, y) => Comparer<int>.Default.Compare(x.SomeInt, y.SomeInt)
+			);
+			Assert.Equal(
+				new[] { "1", "1", "2", "2" },
+				list.ToArray(x => $"{x.SomeInt}")
+			);
+		}
+
+		[Fact]
+		public static void SortByMultipleSelectorWorks()
+		{
+			List<Foo> list;
+
+			list = GetTestList();
+			list.Sort(x => x.SomeInt, x => x.SomeString);
+			Assert.Equal(
+				new[] { "1a", "1b", "2a", "2b" },
+				list.ToArray(x => $"{x.SomeInt}{x.SomeString}")
+			);
+
+
+			list = GetTestList();
+			list.Sort(x => x.SomeString, x => x.SomeInt);
+			Assert.Equal(
+				new[] { "a1", "a2", "b1", "b2" },
+				list.ToArray(x => $"{x.SomeString}{x.SomeInt}")
+			);
+
+			list = GetTestList();
+			list.Sort(x => x.SomeInt, x => x.SomeInt);
+			Assert.Equal(
+				new[] { "1", "1", "2", "2" },
+				list.ToArray(x => $"{x.SomeInt}")
+			);
+		}
+
+		private static List<Foo> GetTestList()
+		{
+			return new List<Foo>()
+			{
+				new Foo() { SomeInt = 2, SomeString = "b" },
+				new Foo() { SomeInt = 1, SomeString = "a" },
+				new Foo() { SomeInt = 1, SomeString = "b" },
+				new Foo() { SomeInt = 2, SomeString = "a" }
+			};
+		}
+
+		private class Foo
+		{
+			public int SomeInt { get; set; }
+			public string SomeString { get; set; }
+		}
+	}
+}
--- Xmf2.Common.Extensions/Collections/SortExtensions.cs
+++ Xmf2.Common.Extensions/Collections/SortExtensions.cs
@@ -10,5 +10,81 @@ namespace Xmf2.Common.Collections
 			list.Sort((a, b) => Comparer<TU>.Default.Compare(sortBySelector(a), sortBySelector(b)));
 			return list;
 		}
+
+		public static void Sort<T>(this List<T> list, params Comparison<T>[] comparisons)
+		{
+			list.Sort(ByCombinedComparisons);
+
+			int ByCombinedComparisons(T x, T y)
+			{
+				int result = 0;
+				for (int i = 0 ; i < comparisons.Length && result == 0 ; i++)
+				{
+					result = comparisons[i](x, y);
+				}
+				return result;
+			}
+		}
+
+		public static void Sort<T, TCompareA, TCompareB>(
+			this List<T> list,
+			Func<T, TCompareA> sortByA,
+			Func<T, TCompareA> thenByB)
+		{
+			var comparerA = Comparer<TCompareA>.Default;
+			var comparerB = Comparer<TCompareA>.Default;
+			list.Sort(
+				(x, y) => comparerA.Compare(sortByA(x), sortByA(y)),
+				(x, y) => comparerB.Compare(thenByB(x), thenByB(y))
+			);
+		}
+
+		public static void Sort<T, TCompareA, TCompareB>(
+			this List<T> list,
+			Func<T, TCompareA> sortByA,
+			Func<T, TCompareB> thenByB)
+		{
+			var comparerA = Comparer<TCompareA>.Default;
+			var comparerB = Comparer<TCompareB>.Default;
+			list.Sort(
+				(x, y) => comparerA.Compare(sortByA(x), sortByA(y)),
+				(x, y) => comparerB.Compare(thenByB(x), thenByB(y))
+			);
+		}
+
+		public static void Sort<T, TCompareA, TCompareB, TCompareC>(
+			this List<T> list,
+			Func<T, TCompareA> sortByA,
+			Func<T, TCompareB> thenByB,
+			Func<T, TCompareC> thenByC)
+		{
+			var comparerA = Comparer<TCompareA>.Default;
+			var comparerB = Comparer<TCompareB>.Default;
+			var comparerC = Comparer<TCompareC>.Default;
+			list.Sort(
+				(x, y) => comparerA.Compare(sortByA(x), sortByA(y)),
+				(x, y) => comparerB.Compare(thenByB(x), thenByB(y)),
+				(x, y) => comparerC.Compare(thenByC(x), thenByC(y))
+			);
+		}
+
+		public static void Sort<T, TCompareA, TCompareB, TCompareC, TCompareD>(
+			this List<T> list,
+			Func<T, TCompareA> sortByA,
+			Func<T, TCompareB> thenByB,
+			Func<T, TCompareC> thenByC,
+			Func<T, TCompareD> thenByD)
+		{
+			var comparerA = Comparer<TCompareA>.Default;
+			var comparerB = Comparer<TCompareB>.Default;
+			var comparerC = Comparer<TCompareC>.Default;
+			var comparerD = Comparer<TCompareD>.Default;
+			list.Sort(
+				(x, y) => comparerA.Compare(sortByA(x), sortByA(y)),
+				(x, y) => comparerB.Compare(thenByB(x), thenByB(y)),
+				(x, y) => comparerC.Compare(thenByC(x), thenByC(y)),
+				(x, y) => comparerD.Compare(thenByD(x), thenByD(y))
+			);
+		}
 	}
 }
