2da5046d9ba513984c999b9933ba0d8be03cca47
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2021-05-17 22:46:39 +0200

    Implementation of GetCombinations

---
+++ Xmf2.Common.Extensions.Tests/Collections/CheckCombineExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xmf2.Common.Collections;
+using Xunit;
+
+namespace Xmf2.Common.Tests.Collections
+{
+	public static class CheckCombineExtensions
+	{
+		[Fact]
+		public static void ShouldFindAllCombinations()
+		{
+			var set = new int [] { 0, 1, 2, 3, 4 };
+			var computedCombinations2 = CombinationExtensions.GetIndexesOfCombination(set.Length, 2).Select(ToStr).ToHashSetOrDie();
+			var computedCombinations3 = CombinationExtensions.GetIndexesOfCombination(set.Length, 3).Select(ToStr).ToHashSetOrDie();
+			var computedCombinations4 = CombinationExtensions.GetIndexesOfCombination(set.Length, 4).Select(ToStr).ToHashSetOrDie();
+			var expectedCombinations2 = new HashSet<string>()
+			{
+				"0,1",
+				"0,2",
+				"0,3",
+				"0,4",
+				"1,2",
+				"1,3",
+				"1,4",
+				"2,3",
+				"2,4",
+				"3,4"
+			};
+			var expectedCombinations3 = new HashSet<string>()
+			{
+				"0,1,2",
+				"0,1,3",
+				"0,1,4",
+				"0,2,3",
+				"0,2,4",
+				"0,3,4",
+				"1,2,3",
+				"1,2,4",
+				"1,3,4",
+				"2,3,4"
+			};
+			var expectedCombinations4 = new HashSet<string>()
+			{
+				"0,1,2,3",
+				"0,1,2,4",
+				"0,1,3,4",
+				"0,2,3,4",
+				"1,2,3,4"
+			};
+
+			Assert.Subset(expectedCombinations2, computedCombinations2);
+			Assert.Subset(expectedCombinations3, computedCombinations3);
+			Assert.Subset(expectedCombinations4, computedCombinations4);
+
+			var charSet = new char[] { 'a', 'b', 'c', 'd', 'e' };
+			var expectedCombinationsA = new HashSet<string>()
+			{
+				"a,b,c,d",
+				"a,b,c,e",
+				"a,b,d,e",
+				"a,c,d,e",
+				"b,c,d,e"
+			};
+			var computedCombinationsA = charSet.GetCombinations(4).Select(ToStr).ToHashSetOrDie();
+			Assert.Subset(expectedCombinationsA, computedCombinationsA);
+		}
+
+		private static string ToStr(IReadOnlyList<char> x) => string.Join(',', x);
+		private static string ToStr(IReadOnlyList<int> x) => string.Join(",", x.Select(x => x.ToString()));
+		private static HashSet<T> ToHashSetOrDie<T>(this IEnumerable<T> enumerable)
+		{
+			var x = new HashSet<T>();
+			foreach (var y in enumerable)
+			{
+				if (x.Contains(y))
+				{
+					throw new Exception("Duplicate");
+				}
+				else
+				{
+					x.Add(y);
+				}
+			}
+			return x;
+		}
+	}
+}
---
+++ Xmf2.Common.Extensions/Collections/CombinationExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace Xmf2.Common.Collections
+{
+	public static class CombinationExtensions
+	{
+		public static IEnumerable<T[]> GetCombinations<T>(this IReadOnlyList<T> collection, int numberInCombination)
+		{
+			foreach (var indexes in GetIndexesOfCombinationInternal(collection.Count, numberInCombination))
+			{
+				var result = new T[numberInCombination];
+				for (int i = 0; i < numberInCombination; i++)
+				{
+					result[i] = collection[indexes[i]];
+				}
+				yield return result;
+			}
+		}
+
+		public static IEnumerable<int[]> GetIndexesOfCombination(int nbElement, int numberInCombination)
+		{
+			foreach (var indexes in GetIndexesOfCombinationInternal(nbElement, numberInCombination))
+			{
+				int[] copy = new int[numberInCombination];
+				indexes.CopyTo(copy, 0);
+				yield return copy;
+			}
+		}
+
+		private static IEnumerable<int[]> GetIndexesOfCombinationInternal(int nbElement, int numberInCombination)
+		{
+			int[] indexes = new int[numberInCombination];
+			int[] indexesMax = new int[numberInCombination];
+			{
+				int j = numberInCombination;
+				for (var i = 0 ; i < numberInCombination ; i++)
+				{
+					indexes[i] = i;
+					indexesMax[i] = nbElement - j--;
+				}
+			}
+			do
+			{
+				yield return indexes;
+			} while (Increment(numberInCombination - 1));
+
+			bool Increment(int indexToIncrement)
+			{
+				if (indexes[indexToIncrement] < indexesMax[indexToIncrement])
+				{
+					indexes[indexToIncrement]++;
+					return true;
+				}
+				else if (indexToIncrement > 0 && Increment(indexToIncrement - 1))
+				{
+					indexes[indexToIncrement] = indexes[indexToIncrement - 1] + 1;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}
+}
