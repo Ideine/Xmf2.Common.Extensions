c8526aa0b2c63a68f7814f730b453cb6f1c0b45a
Christine Lasquellec <christine.lasquellec@ideine.fr>
2023-08-23 09:47:10 +0200

    ajout d'extension verif format email

---
+++ Xmf2.Common.Extensions/Extensions/EmailExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Xmf2.Common.Extensions
+{
+	public static class EmailExtensions
+	{
+		private const string ROUGH_EMAIL_REGEX_PATTERN = "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$";
+		private static readonly Lazy<Regex> ROUGH_EMAIL_REGEX = new(() => new Regex(ROUGH_EMAIL_REGEX_PATTERN, RegexOptions.Compiled));
+
+		public static bool ValidateEmailFormat(this string email) => ROUGH_EMAIL_REGEX.Value.IsMatch(email);
+	}
+}
